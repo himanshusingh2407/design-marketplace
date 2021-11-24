db64e03dda0fd96f3b1a7cafa8e4d2b3066a1e2c
Himanshu Singh <Himanshu.Singh@walmart.com>
2021-11-24 11:13:47 +0530

    fetch images and create infinite scroll view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import React, { useReducer, useRef } from 'react';
+import { getImageUrl } from './Utils';
+import { useFetch, useInfiniteScroll, useLazyLoading } from './customHooks'
 import './App.css';
 
 function App() {
+  const imgReducer = (state, action) => {
+    switch (action.type) {
+      case 'STACK_IMAGES':
+        return { ...state, images: state.images.concat(action.images) }
+      case 'FETCHING_IMAGES':
+        return { ...state, fetching: action.fetching }
+      default:
+        return state;
+    }
+  }
+
+  const pageReducer = (state, action) => {
+    switch (action.type) {
+      case 'ADVANCE_PAGE':
+        return { ...state, page: state.page + 1 }
+      default:
+        return state;
+    }
+  }
+
+  const [pager, pagerDispatch] = useReducer(pageReducer, { page: 0 });
+  const [imgData, imgDispatch] = useReducer(imgReducer, { images: [], fetching: true, });
+
+  let bottomBoundaryRef = useRef(null);
+  useFetch(pager, imgDispatch);
+  useLazyLoading('.card-img-top', imgData.images);
+  useInfiniteScroll(bottomBoundaryRef, pagerDispatch);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="page-title">Design Marketplace</h1>
+      <div id='images'>
+        <div className="row">
+          {imgData.images.map((image, index) => {
+            const { name, thumbnail, id } = image;
+            const imageUrl = getImageUrl(id, thumbnail);
+            return (
+              <div key={index} className="card">
+                <div className="card-body ">
+                  <img
+                    alt={name}
+                    data-src={imageUrl}
+                    className="card-img-top"
+                  />
+                </div>
+                <div className="card-footer">
+                  <div className="card-name">{name}</div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      {imgData.fetching && (
+        <div className="loader">Loading images...</div>
+      )}
+      <div id='page-bottom-boundary' ref={bottomBoundaryRef}></div>
     </div>
   );
 }
